2e4730d2f31b5b040c18801a06f07d2889665639
wangyue <huanying0707@gmail.com>
2020-06-17 12:44:50 +0800

    合约kv语义改成业务语义

--- chaincode/src/shanyioucc/api.go
+++ chaincode/src/shanyioucc/api.go
@@ -4,41 +4,44 @@ import (
 	"fmt"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-protos-go/peer"
-
-	"strings"
 )
 
-func (t *Chaincode) tableGet(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	if len(args) != 2 {
-		return shim.Error("invalid param len")
-	}
-	tableName := args[0]
-	key := args[1]
-	if strings.Contains(tableName, ".") || strings.Contains(key, ".") {
-		return shim.Error("表名或键名含有 '.' 无效字符")
-	}
-	value, err := stub.GetState(tableName + "." + key)
+//善款捐赠
+func (t *Chaincode) addCharityIncome(stub shim.ChaincodeStubInterface, incomeId string, value string) peer.Response {
+	key := "charityIncome" + "." + incomeId
+	err := stub.PutState(key, []byte(value))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	//fmt.Println("set key done", key)
+	fmt.Println("add charity income done", key)
+	return shim.Success([]byte{})
+}
+
+//善款支出
+func (t *Chaincode) addCharityOutcome(stub shim.ChaincodeStubInterface, outcomeId string, value string) peer.Response {
+	key := "charityOutcome" + "." + outcomeId
+	err := stub.PutState(key, []byte(value))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	fmt.Println("add charity outcome done", key)
+	return shim.Success([]byte{})
+}
+
+func (t *Chaincode) getCharityIncome(stub shim.ChaincodeStubInterface, incomeId string) peer.Response {
+	key := "charityIncome" + "." + incomeId
+	value, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	return shim.Success(value)
 }
 
-func (t *Chaincode) tableSet(stub shim.ChaincodeStubInterface, args []string) peer.Response {
-	if len(args) != 3 {
-		return shim.Error("invalid param len")
-	}
-	tableName := args[0]
-	key := args[1]
-	value := []byte(args[2])
-	if strings.Contains(tableName, ".") || strings.Contains(key, ".") {
-		return shim.Error("表名或键名含有 '.' 无效字符")
-	}
-	err := stub.PutState(tableName+"."+key, value)
+func (t *Chaincode) getCharityOutcome(stub shim.ChaincodeStubInterface, outcomeId string) peer.Response {
+	key := "charityOutcome" + "." + outcomeId
+	value, err := stub.GetState(key)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	fmt.Println("set key done", key)
-	return shim.Success([]byte{})
+	return shim.Success(value)
 }
